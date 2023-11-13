570056d89b7bfb950dc8d17361aa4c9b6719d6a6
Joao Wictor <119543998+WictorPereira@users.noreply.github.com>
2023-11-13 17:25:47 -0300

    código de conversor

---
+++ Aula 01 Conversor de moedas/script.js
@@ -0,0 +1,7 @@
+var valorEmDolar = 64
+var cotacaoDoDolar = 5.32
+var nome = 'wictor'
+
+var valoremReal = valorEmDolar * cotacaoDoDolar
+valoremReal = valoremReal.toFixed(2)
+alert('ola ' + nome +' o valor em real e: ' + valoremReal +  ' R$ ')
