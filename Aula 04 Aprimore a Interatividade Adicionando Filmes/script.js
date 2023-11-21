9d48b7c70a1818b9a59fa62ff02f51a2383e0d8c
Joao Wictor <119543998+WictorPereira@users.noreply.github.com>
2023-11-21 11:42:17 -0300

    adicionada função de redirecionamento de trailer no script.js

--- Aula 04 Aprimore a Interatividade Adicionando Filmes/script.js
+++ Aula 04 Aprimore a Interatividade Adicionando Filmes/script.js
@@ -1,8 +1,12 @@
 function adicionarFilme() {
     var filmeFavorito = document.getElementById('filme').value
     var elementoListaFilmes = document.getElementById('listaFilmes')
+    var linkTrailer = document.getElementById('trailer').value
 
-   elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML +`<img src = ${filmeFavorito}>` 
+   elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML +` <a href="${linkTrailer}"> <img src = ${filmeFavorito}></a>` 
    document.getElementById('filme').value = ''
- 
+   document.getElementById('trailer').value = ''
 }
+
+
+
